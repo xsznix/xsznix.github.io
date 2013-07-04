65b3153dc073c15efffbaeb2bdb545db6b6acef6
Xuming Zeng <xsznix@gmail.com>
2013-07-04 17:30:34 -0500

    add lots and lots of real data, including a bunch of pictures

--- rachel-japan/items.js
+++ rachel-japan/items.js
@@ -1,24 +1,61 @@
 $(function() {
 	$(document.body).removeClass('no_js');
-	$('.liquid-slider').liquidSlider({
+	$('#variant_slider').liquidSlider({
 		autoSlide: false,
 		autoHeightEaseFunction: 'easeOutQuint',
 		autoHeightEaseDuration: 1000,
 		slideEaseFunction: 'easeOutQuint',
-		slideEaseDuration: 500
+		slideEaseDuration: 500,
+		mobileNavDefaultText: 'Explore variants...',
+		hashLinking: true
+	});
+	$('#fabric_slider').liquidSlider({
+		autoSlide: false,
+		autoHeightEaseFunction: 'easeOutQuint',
+		autoHeightEaseDuration: 1000,
+		slideEaseFunction: 'easeOutQuint',
+		slideEaseDuration: 500,
+		mobileNavDefaultText: 'Explore fabrics...',
+		hashLinking: true
 	});
 
-	if (is_old_MSIE) {
-		var getImgSize = function (imgSrc, callback) {
-	    var newImg = new Image();
+	// preserve aspect ratio when resizing in IE
+	if (is_old_MSIE) (function() {
+		var getImgSize = function (img, callback) {
+			var newImg = new Image();
 
-	    newImg.onload = function() {
-		    callback.call(newImg, width, height);
-	    };
+			newImg.onload = function() {
+				callback.apply(img, [this.width, this.height]);
+			};
 
-	    newImg.src = imgSrc;
-	  }
+			newImg.src = img.src;
+		}
 
-		// TODO
-	}
+		var allImages = $('img');
+
+		var loadAllImgSizes = function () {
+			allImages.each(function (index, elem) {
+				getImgSize(elem, function (w, h) {
+					$(this).data({aspectRatio: h / w});
+					fixAspectRatio(this);
+				});
+			});
+		}
+
+		var fixAspectRatio = function (elem) {
+				elem.style.height = Math.round((elem.clientWidth ? elem.clientWidth : $(elem).css('max-width')) * $(elem).data('aspectRatio')) + 'px';
+		}
+
+		var fixAllAspectRatios = function () {
+			allImages.each(function (index, elem) {
+				fixAspectRatio(elem);
+			});
+		}
+
+		loadAllImgSizes();
+
+		$(window).resize(function () {
+			fixAllAspectRatios();
+		});
+	})();
 })
